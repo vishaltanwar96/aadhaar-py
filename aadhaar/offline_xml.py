9bab3093cdfff878fc3d682ec4af74e4567ac31d
Vishal Tanwar <vishal.tanwar@outlook.com>
2021-05-14 09:50:53 +0530

    fixed type hints

--- aadhaar/offline_xml.py
+++ aadhaar/offline_xml.py
@@ -1,8 +1,8 @@
 import os
 import zipfile
+from typing import Union
 from operator import getitem
 from functools import reduce
-from typing import Union, BinaryIO
 from collections import OrderedDict
 
 import xmltodict
@@ -13,14 +13,14 @@ from aadhaar.exceptions import EmptyArchiveException, NoXMLFileFound
 
 class AadhaarXMLOffline:
 
-    def __init__(self, path_to_aadhaar_zipfile: Union[str, BinaryIO], share_code: Union[str, int]):
+    def __init__(self, path_to_aadhaar_zipfile: str, share_code: Union[str, int]) -> None:
 
         if not os.path.exists(path_to_aadhaar_zipfile):
             raise IOError("File doesn't exist")
         self.path_to_aadhaar_zipfile = path_to_aadhaar_zipfile
         self.share_code = str(share_code)
 
-    def extract_data(self):
+    def extract_data(self) -> OrderedDict:
 
         with zipfile.ZipFile(self.path_to_aadhaar_zipfile) as xml_zipfile:
             list_of_files = xml_zipfile.namelist()
@@ -37,14 +37,14 @@ class AadhaarXMLOffline:
 
         return self.extract_data()['OfflinePaperlessKyc']['referenceId']
 
-    def validate_email(self, email: str):
+    def validate_email(self, email: str) -> bool:
 
         traversal_list = ['OfflinePaperlessKyc', 'UidData', 'Poi', 'e']
         data = self.extract_data()
         email_hash = reduce(getitem, traversal_list, data)
         return email_hash == generate_sha256_hexdigest(str(email)+self.share_code, int(self.extract_reference_id()[3]))
 
-    def validate_mobile_number(self, mobile_number: Union[str, int]):
+    def validate_mobile_number(self, mobile_number: Union[str, int]) -> bool:
 
         traversal_list = ['OfflinePaperlessKyc', 'UidData', 'Poi', 'm']
         data = self.extract_data()
