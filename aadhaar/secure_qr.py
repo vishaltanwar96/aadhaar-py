07c567b748a69cf902d87923329b0d21c16297ae
vishaltanwar96 <vishal.tanwar@outlook.com>
2021-10-27 21:06:17 +0530

    add method to compress the integer

--- aadhaar/secure_qr.py
+++ aadhaar/secure_qr.py
@@ -1,3 +1,6 @@
 class SecureQRCodeCompressedBytesData:
     def __init__(self, scanned_integer_data: int) -> None:
         self._scanned_integer_data = scanned_integer_data
+
+    def compress(self) -> bytes:
+        return self._scanned_integer_data.to_bytes(length=16 * 1_024, byteorder="big")
--- tests/unit/test_secure_qr.py
+++ tests/unit/test_secure_qr.py
@@ -4,6 +4,8 @@ from aadhaar.secure_qr import SecureQRCodeCompressedBytesData
 
 
 class TestQRCodeCompressedBytesData(TestCase):
-    def test_has_qr_code_compressed_bytes_data_class(self) -> None:
-        scanned_integer_data = 12345
-        SecureQRCodeCompressedBytesData(scanned_integer_data)
+    def test_has_qr_code_compressed_bytes_data_class_has_method_to_compress_integer(
+        self,
+    ) -> None:
+        compressed_bytes = SecureQRCodeCompressedBytesData(12345)
+        compressed_bytes.compress()
