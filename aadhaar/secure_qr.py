310fa3d6970550666b1861be7cff449bd505b2b1
vishaltanwar96 <vishal.tanwar@outlook.com>
2021-10-30 17:02:50 +0530

    implement decompress method

--- aadhaar/secure_qr.py
+++ aadhaar/secure_qr.py
@@ -1,3 +1,10 @@
+import zlib
+
+
+class MalformedDataReceived(Exception):
+    pass
+
+
 class SecureQRCodeScannedInteger:
     def __init__(self, data: int) -> None:
         self._data = data
@@ -10,5 +17,18 @@ class SecureQRCompressedBytesData:
     def __init__(self, data: bytes) -> None:
         self._data = data
 
+    def _remove_null_bytes_from_left(self) -> bytes:
+        return self._data.lstrip(b"\x00")
+
     def decompress(self):
-        pass
+        bytes_data = self._remove_null_bytes_from_left()
+        try:
+            decompressed_bytes_data = zlib.decompress(
+                bytes_data,
+                wbits=zlib.MAX_WBITS + 15,
+            )
+        except zlib.error:
+            raise MalformedDataReceived(
+                "Decompression failed, Please provide valid data.",
+            )
+        return decompressed_bytes_data
--- tests/unit/test_secure_qr.py
+++ tests/unit/test_secure_qr.py
@@ -1,6 +1,11 @@
+import pathlib
 from unittest import TestCase
 
-from aadhaar.secure_qr import SecureQRCodeScannedInteger, SecureQRCompressedBytesData
+from aadhaar.secure_qr import (
+    MalformedDataReceived,
+    SecureQRCodeScannedInteger,
+    SecureQRCompressedBytesData,
+)
 
 
 class TestSecureQRCodeScannedInteger(TestCase):
@@ -17,6 +22,27 @@ class TestSecureQRCodeScannedInteger(TestCase):
 
 
 class TestSecureQRCompressedBytesData(TestCase):
-    def test_has_decompress_method(self) -> None:
-        compressed_bytes_data = SecureQRCompressedBytesData(b"\x00")
-        compressed_bytes_data.decompress()
+    def setUp(self) -> None:
+        qr_data_int = self._prepare_test_qr_code_data()
+        dummy_int = 1234567
+        self.qr_data_bytes = qr_data_int.to_bytes(length=16 * 1024, byteorder="big")
+        self.dummy_bytes_data = dummy_int.to_bytes(length=16 * 1024, byteorder="big")
+
+    def _prepare_test_qr_code_data(self) -> int:
+        current_file = pathlib.Path(__file__).resolve()
+        project_root = current_file.parent.parent.parent
+        with open(
+            project_root / "test_data" / "secure_qr_data.txt",
+        ) as sample_data_file:
+            sample_data = sample_data_file.read()
+        return int(sample_data)
+
+    def test_contains_expected_data_in_decompressed_bytes_data(self) -> None:
+        expected_data = b"Penumarthi"
+        compressed_bytes_data = SecureQRCompressedBytesData(self.qr_data_bytes)
+        self.assertIn(expected_data, compressed_bytes_data.decompress())
+
+    def test_raises_exception_when_given_dummy_data(self) -> None:
+        compressed_bytes_data = SecureQRCompressedBytesData(self.dummy_bytes_data)
+        with self.assertRaises(MalformedDataReceived):
+            compressed_bytes_data.decompress()
